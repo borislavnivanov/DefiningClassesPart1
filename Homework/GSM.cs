672a1db35eea2073b524bf6d7dcff9d9f7cc07b1
Borislav Nikolaev Ivanov <borislavnivanov@gmail.com>
2016-12-20 00:13:01 +0200

    fsdh

---
+++ Homework/GSM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class GSM
+    {
+        private string model;
+        private string manufacture;
+        private string owner;
+
+
+
+        class Battery
+        {
+            private string model;
+            private string iddleTime;
+            private string TalkTime;
+        }
+
+        class Display
+        {
+            private int size;
+            private int colours;
+        }
+
+        public enum BatteryType
+        {
+            LiIon,NiMH, NiCd,Other
+        }
+    }
+}
